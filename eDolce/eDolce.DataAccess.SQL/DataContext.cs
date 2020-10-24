03319962a179c7d5daa5e7c8860385c6b730d00f
Simanga Shabalala <simangae@gmail.com>
2020-10-24 10:41:20 +0200

    Added Unit.MVC, EntityFramework and Migrations

---
+++ eDolce/eDolce.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using eDolce.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eDolce.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductCategory> productCategories { get; set; }
+    }
+}
---
+++ eDolce/eDolce.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace eDolce.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<eDolce.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(eDolce.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
